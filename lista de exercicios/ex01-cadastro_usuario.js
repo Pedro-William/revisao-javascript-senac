487e7310178aac46512662b10b6866c144335ddb
Pedro William <124735242+Pedro-William@users.noreply.github.com>
2026-08-18 21:15:42 -0300

    Delete lista de exercicios directory

--- lista de exercicios/ex01-cadastro_usuario.js
+++
@@ -1,8 +0,0 @@
-const nome = "Pedro"
-let idade = "20"
-const email = "pedro.sampaio@gmail.com"
-let estaLogado = true
-let ultimoAcesso = null
-
-console.log(`Nome: ${nome} | idade: ${idade}`)
-console.log(typeof(nome))
